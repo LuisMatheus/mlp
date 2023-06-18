d7c81fb66fdd0659e8fa9e7b82e6ff35e0224eae
Matheus Tavares <luismatheus.tavares@gmail.com>
2023-06-17 21:16:36 -0300

    changed code

--- mlp.py
+++ mlp.py
@@ -1,93 +1,121 @@
-import numpy as np
+from math import exp
+from random import seed
+from random import random
+
+# Initialize a network
 
 
-class layer_dense:
-    def __init__(self, weights, neurons):
-        self.weights = 0.10 * np.random.randn(weights, neurons)
-        self.neurons = np.ones((1, neurons))
+def initialize_network(n_inputs, n_hidden, n_outputs):
+    network = list()
+    hidden_layer = [{'weights': [random() for i in range(n_inputs + 1)]}
+                    for i in range(n_hidden)]
+    network.append(hidden_layer)
+    output_layer = [{'weights': [random() for i in range(n_hidden + 1)]}
+                    for i in range(n_outputs)]
+    network.append(output_layer)
+    return network
 
-    def forward(self, input):
-        self.output = np.dot(input, self.weights) + self.neurons
-
-
-class activation:
-    def forward(self, input):
-        self.output = 1.0 / (1.0 + np.exp(-input))
-
-
-class activation_softmax:
-    def forward(self, input):
-        exp_values = np.exp(input - np.max(input, axis=1, keepdims=True))
-        self.output = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+# Calculate neuron activation for an input
 
 
-class loss:
-    def calculate(self, output, y):
-        sample_losses = self.forward(output, y)
-        return np.mean(sample_losses)
+def activate(weights, inputs):
+    activation = weights[-1]
+    for i in range(len(weights)-1):
+        activation += weights[i] * inputs[i]
+    return activation
+
+# Transfer neuron activation
 
 
-class loss_cross_entropy(loss):
-    def forward(self, y_pred, y_true):
-        samples = len(y_pred)
-        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
-        correct_confidence = y_pred_clipped[range(samples), y_true]
-        return -np.log(correct_confidence)
+def transfer(activation):
+    return 1.0 / (1.0 + exp(-activation))
+
+# Forward propagate input to a network output
 
 
-class mlp:
-    def __init__(self, input, output, hidden_layers=[1, 2, 3, 4], max_epochs=100, learning_rate=0.1):
-        self.input = input
-        self.output = output
-        self.learning_rate = learning_rate
-        self.hidden_layers = hidden_layers
-        self.max_epochs = max_epochs
+def forward_propagate(network, row):
+    inputs = row
+    for layer in network:
+        new_inputs = []
+        for neuron in layer:
+            activation = activate(neuron['weights'], inputs)
+            neuron['output'] = transfer(activation)
+            new_inputs.append(neuron['output'])
+        inputs = new_inputs
+    return inputs
 
-        # init weights
-        layers = []
-        layers.append(layer_dense(input.shape[1], input.shape[1]))
-        print("criado primeiro layer")
-        for i in range(len(hidden_layers)):
-            print("Criando layer", i)
-            layers.append(layer_dense(
-                layers[i].neurons.shape[1], hidden_layers[i]))
-        layers.append(layer_dense(hidden_layers[-1], output.shape[1]))
-        print("Criado ultimo layer")
-
-        # init functions
-        activations = []
-        for i in range(len(hidden_layers)):
-            activations.append(activation())
-        activations.append(activation_softmax())
-        self.activation = activations
-
-        self.layers = layers
-
-    def train(self):
-        print("Initializing the train process....")
-
-        epochs = 0
-        while epochs < self.max_epochs:
-            print(f"[EPOCH] {epochs} ")
-
-            for x, d in zip(self.input, self.output):
-
-                self.layers[0].forward(x)
-                self.activation[0].forward(self.layers[0].output)
-
-                for i in range(1, len(self.layers)-1):
-                    self.layers[i].forward(self.layers[i-1].output)
-                    self.activation[i].forward(self.layers[i].output)
-
-                self.layers[-1].forward(self.layers[-2].output)
-                self.activation[-1].forward(self.layers[-1].output)
-
-                print(f"output: {self.layers[-1].output} d: {d}")
-            epochs = epochs + 1
+# Calculate the derivative of an neuron output
 
 
-input = np.array([[0.1, 0.2], [0.3, 0.4]])
-labels = np.array([[0, 1, 0, 0], [1, 0, 0, 0]])
-hidden_layers = [1, 2, 3]
-test = mlp(input, labels, hidden_layers=hidden_layers)
-test.train()
+def transfer_derivative(output):
+    return output * (1.0 - output)
+
+# Backpropagate error and store in neurons
+
+
+def backward_propagate_error(network, expected):
+    for i in reversed(range(len(network))):
+        layer = network[i]
+        errors = list()
+        if i != len(network)-1:
+            for j in range(len(layer)):
+                error = 0.0
+                for neuron in network[i + 1]:
+                    error += (neuron['weights'][j] * neuron['delta'])
+                errors.append(error)
+        else:
+            for j in range(len(layer)):
+                neuron = layer[j]
+                errors.append(neuron['output'] - expected[j])
+        for j in range(len(layer)):
+            neuron = layer[j]
+            neuron['delta'] = errors[j] * transfer_derivative(neuron['output'])
+
+# Update network weights with error
+
+
+def update_weights(network, row, l_rate):
+    for i in range(len(network)):
+        inputs = row[:-1]
+        if i != 0:
+            inputs = [neuron['output'] for neuron in network[i - 1]]
+        for neuron in network[i]:
+            for j in range(len(inputs)):
+                neuron['weights'][j] -= l_rate * neuron['delta'] * inputs[j]
+            neuron['weights'][-1] -= l_rate * neuron['delta']
+
+# Train a network for a fixed number of epochs
+
+
+def train_network(network, train, l_rate, n_epoch, n_outputs):
+    for epoch in range(n_epoch):
+        sum_error = 0
+        for row in train:
+            outputs = forward_propagate(network, row)
+            expected = [0 for i in range(n_outputs)]
+            expected[row[-1]] = 1
+            sum_error += sum([(expected[i]-outputs[i]) **
+                             2 for i in range(len(expected))])
+            backward_propagate_error(network, expected)
+            update_weights(network, row, l_rate)
+        print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
+
+
+# Test training backprop algorithm
+seed(1)
+dataset = [[2.7810836, 2.550537003, 0],
+           [1.465489372, 2.362125076, 0],
+           [3.396561688, 4.400293529, 0],
+           [1.38807019, 1.850220317, 0],
+           [3.06407232, 3.005305973, 0],
+           [7.627531214, 2.759262235, 1],
+           [5.332441248, 2.088626775, 1],
+           [6.922596716, 1.77106367, 1],
+           [8.675418651, -0.242068655, 1],
+           [7.673756466, 3.508563011, 1]]
+n_inputs = len(dataset[0]) - 1
+n_outputs = len(set([row[-1] for row in dataset]))
+network = initialize_network(n_inputs, 2, n_outputs)
+train_network(network, dataset, 0.5, 20, n_outputs)
+for layer in network:
+    print(layer)
