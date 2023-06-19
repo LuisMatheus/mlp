91ad2818a8102a81dca3517be308c42df3a9b198
Matheus Tavares <luismatheus.tavares@gmail.com>
2023-06-19 00:49:54 -0300

    added accuracy and early stop

--- mlp.py
+++ mlp.py
@@ -76,12 +76,13 @@ def update_weights(network, row, l_rate):
             neuron['weights'][-1] -= l_rate * neuron['delta']
 
 
-def train_network(network, x_train, y_train, l_rate, n_epoch, es_threshold=0.01):
+def train_network(network, x_train, y_train, x_test, y_test, l_rate, n_epoch, es_threshold=0.01):
     errors = []
+    accuracies = []
     epoch = 0
-    sum_error = -1
-    while epoch < n_epoch or sum_error > es_threshold:
+    while epoch < n_epoch:
         sum_error = 0
+        accuracy = 0
         for X, y in zip(x_train, y_train):
             predicted_outputs = forward_propagate(network, X)
             sum_error += sum([(y[i]-predicted_outputs[i])
@@ -90,8 +91,41 @@ def train_network(network, x_train, y_train, l_rate, n_epoch, es_threshold=0.01)
             update_weights(network, X, l_rate)
 
         errors.append(sum_error)
-        print(f"[EPOCH] {epoch} - Error: {sum_error}")
+        accuracy = get_accuracy(network, x_test, y_test)
+        accuracies.append(accuracy)
+        print(
+            f"[EPOCH] {epoch} - Error: {sum_error} - Accuracy: {accuracy}")
+
+        # early stopping
+        if epoch > 1 and abs(accuracies[-2] - accuracies[-1]) <= es_threshold:
+            print("Early Stopping")
+            break
+
         epoch += 1
+    return errors, accuracies
+
+
+def train_test_split(x, y, test_size, random_state=None):
+    np.random.seed(random_state)
+    new_index = np.random.permutation(len(x))
+    test_size = int(test_size * len(x))
+    test_index = new_index[:test_size]
+    train_index = new_index[test_size:]
+    x_train = [x[i] for i in train_index]
+    y_train = [y[i] for i in train_index]
+    x_test = [x[i] for i in test_index]
+    y_test = [y[i] for i in test_index]
+    return np.array(x_train, np.float64), np.array(y_train, np.float64), \
+        np.array(x_test, np.float64), np.array(y_test, np.float64)
+
+
+def get_accuracy(network, x_test, y_test):
+    correct = 0
+    for X, y in zip(x_test, y_test):
+        outputs = forward_propagate(network, X)
+        if np.argmax(outputs) == np.argmax(y):
+            correct += 1
+    return correct / len(x_test)
 
 
 def predict(network, row):
@@ -115,12 +149,17 @@ labels = np.where(labels == -1, 0, labels)
 n_inputs = len(dataset[0])
 n_outputs = len(labels[0])
 
-n_layers = 20
-l_rate = 0.2
-n_epoch = 100
-es_threshold = 0.01
+x_train, y_train, x_test, y_test = train_test_split(
+    dataset, labels, test_size=0.2
+)
+
+n_layers = 5
+l_rate = 0.01
+n_epoch = 20
+# early stopping - acuracy
+es_threshold = 0.001
+
 
 network = initialize_network(n_inputs, n_layers, n_outputs)
-train_network(network, dataset, labels, l_rate, n_epoch, es_threshold)
-# for layer in network:
-#   pass
+train_network(network, x_train, y_train, x_test,
+              y_test, l_rate, n_epoch, es_threshold)
